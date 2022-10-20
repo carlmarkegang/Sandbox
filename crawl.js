311b519c5b1227d30a5a85816b5e8264a4d78b87
Carl Markegång <huledule@hotmail.com>
2022-10-20 13:00:42 +0200

    Update crawl.js

--- crawl.js
+++ crawl.js
@@ -16,3 +16,28 @@ function visitLink(index) {
 }
 
 visitLink(0);
+
+
+//------------------------------------------------------
+// Open instead of iframe:
+var links_to_visit = [];
+var start = new Date().toLocaleTimeString();
+$("#_desktop_top_menu a").each(function (index) {
+    if (links_to_visit[index] != "" || links_to_visit[index] != undefined) {
+        links_to_visit.push($(this).attr('href'));
+    }
+});
+
+function visitLink(index) {
+    if (index < links_to_visit.length) {
+        var nextIndex = index += 1;
+        var current = new Date().toLocaleTimeString();
+        window.open(links_to_visit[index]);
+        console.log(index + "/" + links_to_visit.length + " Start: " + start + " Last update: " + current);
+        setTimeout(function () {
+            visitLink(nextIndex)
+        }, 1000);
+    }
+}
+
+visitLink(0);
